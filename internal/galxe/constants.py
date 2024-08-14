45cd4cbb2c75027344a72f7c7158e624f6d63127
Tawer10 <92242421+Tawer10@users.noreply.github.com>
2024-08-14 13:55:36 +0300

    Update constants.py

--- internal/galxe/constants.py
+++ internal/galxe/constants.py
@@ -4,3 +4,12 @@ GALXE_DISCORD_CLIENT_ID = '947863296789323776'
 CHAIN_NAME_MAPPING = {'MATIC': 'Polygon'}
 
 VERIFY_TRIES = 4
+
+QUIZ_CHOICES = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+}
